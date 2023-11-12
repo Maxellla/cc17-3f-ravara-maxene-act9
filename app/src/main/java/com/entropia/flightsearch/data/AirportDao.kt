8f01ce7cb5ef0ad2fa54b7c3cb84f62628aeb12d
entropia <marta9kut@gmail.com>
2023-11-12 16:19:36 +0100

    added Daos Tests

--- app/src/main/java/com/entropia/flightsearch/data/AirportDao.kt
+++ app/src/main/java/com/entropia/flightsearch/data/AirportDao.kt
@@ -7,7 +7,7 @@ import kotlinx.coroutines.flow.Flow
 @Dao
 interface AirportDao {
 
-    @Query("SELECT * FROM airport WHERE iata_code LIKE :input+'%' OR name LIKE '%'+ :input + '%'")
+    @Query("SELECT * FROM airport WHERE iata_code LIKE :input OR name LIKE :input")
     fun getAirportByName(input: String): Flow<List<Airport>>
 
     @Query("SELECT * FROM airport ORDER BY passengers DESC")
--- app/src/main/java/com/entropia/flightsearch/data/FavoriteDao.kt
+++ app/src/main/java/com/entropia/flightsearch/data/FavoriteDao.kt
@@ -1,11 +1,20 @@
 package com.entropia.flightsearch.data
 
 import androidx.room.Dao
+import androidx.room.Delete
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
 import androidx.room.Query
 import kotlinx.coroutines.flow.Flow
 
 @Dao
 interface FavoriteDao {
+
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun addFavorite(favorite: Favorite)
+
+    @Delete
+    suspend fun removeFavorite(favorite: Favorite)
 
     @Query("SELECT * FROM favorite ORDER BY departure_code ASC")
     fun getAll(): Flow<List<Favorite>>
